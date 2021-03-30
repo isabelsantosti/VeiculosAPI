ea3fa9c093f9755db7bf5ca83b628d8260b575af
fernando Schilipack <nando_fernendo@hotmail.com>
2021-03-30 11:21:21 -0300

    criado a classe model para a tabela de veiculos

---
+++ Models/Veiculos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeiculosAPI.Models
+{
+    public class Veiculos
+    {
+        public int Id_veiculo { get; set; }
+        public string Nome_veiculo { get; set; }
+        public double Preco { get; set; }
+    }
+}
