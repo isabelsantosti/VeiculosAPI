ed5ab6f2a4bbda39a2f798b7b7554b71294a6a24
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-08 14:43:37 -0300

    criado a model class Troca Senha

---
+++ Models/TrocarSenha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeiculosAPI.Models
+{
+    public class TrocarSenha
+    {
+        public string SenhaAntiga { get; set; }
+        public string ConfirmarSenha { get; set; }
+        public string NovaSenha { get; set; }
+    }
+}
