8d91e32d6646bb73f73d93217207f022744b66f0
fernando Schilipack <nando_fernendo@hotmail.com>
2021-04-12 14:06:35 -0300

    controller para adicionar fotos veiculos

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,57 @@
+﻿using ImageUploader;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using VeiculosAPI.Data;
+using VeiculosAPI.Models;
+
+namespace VeiculosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private SVTADbContext _svtaDbContext;
+
+        public ImagesController(SVTADbContext sVTADbContext)
+        {
+            _svtaDbContext = sVTADbContext;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Post([FromBody]Imagem Imagens)
+        {
+            var usuarioEmail = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value;
+            var usuario = _svtaDbContext.Usuarios.FirstOrDefault(u => u.Email == usuarioEmail);
+            if (usuario == null)
+                return NotFound("Usuario não encontrado");
+            var stream = new MemoryStream(Imagens.ImageArray);
+            //globally unique identifier = GUID serve para dar um id unico para o arquivo, evitando sua duplicação
+            var guid = Guid.NewGuid().ToString();
+            var arquivo = $"{guid}.jpg";
+            var pasta = "wwwroot/Images";
+            var resposta = FilesHelper.UploadImage(stream, pasta, arquivo);
+            if (!resposta)
+                return BadRequest();
+            else
+            {
+                var imageObj = new Imagem()
+                {
+                    ImageUrl = arquivo,
+                    VeiculoId = Imagens.VeiculoId
+                };
+                _svtaDbContext.Imagens.Add(imageObj);
+                _svtaDbContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created);
+            }
+        }
+
+    }
+}
--- Models/Imagem.cs
+++ Models/Imagem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,5 +11,7 @@ namespace VeiculosAPI.Models
         public int Id { get; set; }
         public string ImageUrl { get; set; }
         public int VeiculoId { get; set; }
+        [NotMapped]
+        public byte[] ImageArray { get; set; }
     }
 }
